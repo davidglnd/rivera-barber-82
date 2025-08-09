f74f66c01c9173689585d0c626f64abb16785e21
davidglnd <david.galindo.dgp@gmail.com>
2025-08-09 17:24:55 +0200

    feat(AppointmentFinder): :sparkles: Boton funcionando , llegas a la vista
    Aun queda la reunion con el cliente para saber como quiere resolverlo

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const finderButton = document.querySelector('#finder-button');
+
+    finderButton.addEventListener('click', () => window.location.href = '../pages/appointment-finder.html');
+});
